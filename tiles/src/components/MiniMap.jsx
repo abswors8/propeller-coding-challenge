e32fd100c5bced4651eedc07a21d2cf9e5b262cc
Abigail Worswick <abbieworswick@Abigails-MacBook-Air.local>
2025-06-09 13:23:02 +1000

    adding lint and blocking tests

--- tiles/src/components/MiniMap.jsx
+++ tiles/src/components/MiniMap.jsx
@@ -3,44 +3,42 @@ import Tile from './Tile';
 const OVERVIEW_SIZE = 200;
 
 export default function MiniMap({miniMapRef, setIsDraggingViewport, handleClick, position, zoom, viewportWidth, viewportHeight, tileSize}) {
-    const fullMapSize = (2 ** zoom) * tileSize;
+  const fullMapSize = (2 ** zoom) * tileSize;
 
-    const viewportRect = {
-        left: (position.x / fullMapSize) * OVERVIEW_SIZE,
-        top: (position.y / fullMapSize) * OVERVIEW_SIZE,
-        width: (viewportWidth / fullMapSize) * OVERVIEW_SIZE,
-        height: (viewportHeight / fullMapSize) * OVERVIEW_SIZE,
-    };
-    
-    return (
-        <div
-        ref={miniMapRef}
-        className="fixed bottom-4 right-4 z-50 border border-gray-400 rounded cursor-default overflow-hidden shadow-md bg-[#222]"
+  const viewportRect = {
+    left: (position.x / fullMapSize) * OVERVIEW_SIZE,
+    top: (position.y / fullMapSize) * OVERVIEW_SIZE,
+    width: (viewportWidth / fullMapSize) * OVERVIEW_SIZE,
+    height: (viewportHeight / fullMapSize) * OVERVIEW_SIZE,
+  };
+
+  return (
+    <div
+      ref={miniMapRef}
+      className="fixed bottom-4 right-4 z-50 border border-gray-400 rounded cursor-default overflow-hidden shadow-md bg-[#222]"
+      style={{
+        width: OVERVIEW_SIZE,
+        height: OVERVIEW_SIZE,
+      }}
+      onClick={handleClick}
+    >
+      <Tile
+        className="w-full h-full object-cover"
+        zoom={0}
+        x={0}
+        y={0}
+        tileSize={OVERVIEW_SIZE}
+      />
+      <div
+        className="absolute border-2 border-primary bg-primary/30 box-border rounded"
         style={{
-          width: OVERVIEW_SIZE,
-          height: OVERVIEW_SIZE,
+          left: viewportRect.left,
+          top: viewportRect.top,
+          width: Math.min(viewportRect.width, OVERVIEW_SIZE-2),
+          height: Math.min(viewportRect.height, OVERVIEW_SIZE-2),
         }}
-        onClick={handleClick}
-      >
-        <Tile
-          className="w-full h-full object-cover"
-          zoom={0}
-          x={0}
-          y={0}
-          tileSize={OVERVIEW_SIZE}
-        />
-  
-        {/* Viewport indicator */}
-        <div
-          className="absolute border-2 border-primary bg-primary/30 box-border rounded"
-          style={{
-            left: viewportRect.left,
-            top: viewportRect.top,
-            width: Math.min(viewportRect.width, OVERVIEW_SIZE-2),
-            height: Math.min(viewportRect.height, OVERVIEW_SIZE-2),
-          }}
-          onMouseDown={() => setIsDraggingViewport(true)}
-        />
-      </div>
-    );
+        onMouseDown={() => setIsDraggingViewport(true)}
+      />
+    </div>
+  );
 }
